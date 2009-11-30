035a33b152a3c8b734a7acbeea6b175835ab4831
Greg Leuch <contact@gleuch.com>
2009-11-29 22:39:16 -0800

    adding commify method.

--- tweet.rb
+++ tweet.rb
@@ -159,6 +159,36 @@ helpers do
   end
   def possessiveify(str); str.match(/s$/i) ? "#{str}'" : "#{str}'s"; end
 
+  def commify(n, max=false, title='', char='%.01f', plus=true) # Courtesy of Magma <http://mag.ma>
+    n.to_s =~ /([^\.]*)(\..*)?/
+    int, dec = $1.reverse, $2 ? $2 : ""
+    while int.gsub!(/(,|\.|^)(\d{3})(\d)/, '\1\2,\3'); end
+    int = int.reverse + dec
+
+    if (max && (max === true || max <= n.to_i) && n.to_i >= 1000)
+      if (n.to_i >= 1000000000)
+        num = sprintf(char, (n.to_i/100000000.to_f))
+        ext = 'b'
+      elsif (n.to_i >= 1000000)
+        num = sprintf(char, (n.to_i/1000000.to_f))
+        ext = 'm'
+      else (n.to_i >= 1000)
+        num = sprintf(char, (n.to_i/1000.to_f))
+        ext  = 'k'
+      end
+    
+      num.to_s =~ /([^\.]*)(\..*)?/
+      num, dec = $1.reverse, $2 ? $2 : ""
+      while num.gsub!(/(,|\.|^)(\d{3})(\d)/, '\1\2,\3')
+      end
+      num = num.reverse + dec
+    
+      str = "#{num}<span class='num_ext'>#{ext}</span>#{plus ? "<span class='num_plus'>+</span>" : ''}"
+    else
+      str = int
+    end
+    "<span title='#{int} #{title}'>#{str}</span>"
+  end
 end #helpers
 
 
